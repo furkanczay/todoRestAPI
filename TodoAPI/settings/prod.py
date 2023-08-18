93aec8b4dd5fa05f3b68a750ca365dbd7745c095
Furkan Özay <furkann.ozay@hotmail.com>
2023-08-18 18:26:02 +0300

    prod settings güncellendi

--- TodoAPI/settings/prod.py
+++ TodoAPI/settings/prod.py
@@ -17,5 +17,19 @@ ALLOWED_HOSTS += ["todorestapi-20432433159e.herokuapp.com"]
 
 INSTALLED_APPS += [
     'rest_framework',
-    'todos'
+    'todos',
+    'corsheaders',
+]
+
+MIDDLEWARE += [
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5500',
+    'http://localhost:5501',
+    'http://localhost:5502',
+    'http://localhost:5503',
+    'http://localhost:5504',
+    'http://localhost:5505',
 ]
